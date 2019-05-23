c54bde6bf203274399d665e17b96e702e9166a29
Trevahr Robertson <trevahr@yahoo.com>
2019-05-23 19:07:22 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,30 +3,43 @@ def begins_with_r(arr)
   r = []
   arr.each do |word|
   
+
   
    if word.start_with?('r')
      r.push(true)
    else
      r.push(false)
      end
+    a = word.split("")
+    if a[0] == "r"
+     r.push(true)
+   else
+     r.push(false)
+    end
   end
   r.all?
 end
+
+
+
 
 def contain_a(arr)
   a = []
     arr.each do |word|
      
-      contain = word.split("")
-            if  contain.include?("a")
+
+       contain = word.split("")
+             if  contain.include?("a")
                 a.push(word)
-          end
+        end
     end
     a
+
 end
 
 def first_wa
   
   
   
+
 end
